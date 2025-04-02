0acabe27d9fe38293aca182e8b9135969f7914d0
jayesh787 <119455931+jayesh787@users.noreply.github.com>
2025-04-02 16:58:04 +0000

    variable _2

---
+++ JS/variable.js
@@ -0,0 +1,24 @@
+// const accountId = 123
+// let accountEmail = "jayesh@gmail.com"
+// var accountPassword = "jayesh123"
+// let accountCity = "Jaipur"
+
+// accountEmail = "akajshbajhx@gmail.com"
+// accountPassword = "sikdhiujsgdiuy"
+// accountCity = "goa"
+
+// console.log(accountEmail);
+
+
+// console.table({accountId, accountEmail, accountPassword, accountCity }) 
+
+
+
+
+const accountId = 1234
+let accountEmail ="kdshsuijdud "
+
+// accountId = 878787
+ accountEmail = "akbshujaxyhuvfy@gmail.com"
+
+console.table({accountId, accountEmail})
